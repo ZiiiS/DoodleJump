f59356ccf7a1104849b85dc4e06beb55dff5ed4e
ZiiiS <Alexc_d99@mail.ru>
2021-10-20 17:55:54 +0300

    initial commit

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+	// Ускорение игрока
+	[Header("Player velocity")]
+	// Ось Ox
+	public int xVelocity = 3;
+	// Ось Oy
+	public int yVelocity = 3;
+
+	[SerializeField] private LayerMask ground;
+
+	private bool isFacingRight = true;
+
+	// Физическое поведение (тело) объекта
+	private Rigidbody2D rigidBody;
+	// Коллайдер, проверка на столкновения
+	private Collider2D coll;
+
+	private void Start()
+	{
+		// Получаем доступ к Rigidbody2D объекта Player
+		rigidBody = gameObject.GetComponent<Rigidbody2D>();
+		coll = gameObject.GetComponent<Collider2D>();
+	}
+
+	private void Update()
+	{
+		updatePlayerPosition();
+	}
+
+	// Обновляем местоположение игрока
+	private void updatePlayerPosition()
+	{
+		// Получаем значение ввода горизонтального перемещение
+		float moveInput = Input.GetAxis("Horizontal");
+
+		// Значения xVelocity, yVelocity можно задать через инспектор
+		if (moveInput < 0)
+		{ // Движ влево
+			rigidBody.velocity = new Vector2(-xVelocity, rigidBody.velocity.y);
+		}
+		else if (moveInput > 0)
+		{ // Движ вправо
+			rigidBody.velocity = new Vector2(xVelocity, rigidBody.velocity.y);
+		}
+		else if (moveInput == 0)
+		{
+			rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+		}
+		else if (coll.IsTouchingLayers(ground))
+		{
+			rigidBody.velocity = Vector2.zero; // Лично меня дико бесит инерция вбок при приземлении персонажа, отключаем
+		}
+
+		if (coll.IsTouchingLayers(ground))
+		{ //Тип прыгает, если стоит на земле
+			rigidBody.velocity = new Vector2(rigidBody.velocity.x, yVelocity);
+		}
+		if (moveInput > 0 && !isFacingRight)
+			//отражаем персонажа вправо
+			Flip();
+		//обратная ситуация. отражаем персонажа влево
+		else if (moveInput < 0 && isFacingRight)
+			Flip();
+
+
+		
+	}
+	private void Flip()
+	{
+		//меняем направление движения персонажа
+		isFacingRight = !isFacingRight;
+		//получаем размеры персонажа
+		Vector3 theScale = transform.localScale;
+		//зеркально отражаем персонажа по оси Х
+		theScale.x *= -1;
+		//задаем новый размер персонажа, равный старому, но зеркально отраженный
+		transform.localScale = theScale;
+	}
+}
+
+//
+//if (rigidbody.transform.position.x < -3.5)
+//	rigidbody.transform.position = new vector2(transform.position.x + 20f, transform.position.y);
+//else if (transform.position.x > 3.5)
+//	rigidbody.transform.position = new vector2(transform.position.x - 20f, transform.position.y);
